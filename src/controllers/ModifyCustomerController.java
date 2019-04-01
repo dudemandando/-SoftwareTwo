71b1b24af3fef69f8b14ca07ebdbc40d16887161
Dan <danburke0@outlook.com>
2019-03-31 17:08:16 -0700

    Added FXML linked up save and cancel buttons

--- src/controllers/ModifyCustomerController.java
+++ src/controllers/ModifyCustomerController.java
@@ -7,21 +7,56 @@ package controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
- * @author Megatron
+ * @author Dan Burke
  */
 public class ModifyCustomerController implements Initializable {
 
     /**
      * Initializes the controller class.
      */
+    
+    @FXML AnchorPane anchorPane;
+    @FXML TextField customerName;
+    @FXML TextField addressOne;
+    @FXML TextField addressTwo;
+    @FXML TextField city;
+    @FXML TextField postalCode;
+    @FXML TextField country;
+    @FXML TextField phoneNumber;
+    @FXML RadioButton isActive;
+    @FXML Button saveButton;
+    @FXML Button cancelButton;
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    @FXML
+    public void onSaveButton(){
+        System.out.println("Modify Customer Save Button");
+        
+    }
+    
+    @FXML
+    public void onCancelButton(){
+        System.out.println("Modify Customer Cancel Button");
+    }
+    
+    private void populateInfo(){
+        
+    }
     
 }
