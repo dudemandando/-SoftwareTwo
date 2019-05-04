1559d182029bc6b4604cb6d729f21a976060da93
Dan <danburke0@outlook.com>
2019-05-04 16:15:30 -0700

    returns to list of all customers to make a app

--- src/controllers/CreateAppController.java
+++ src/controllers/CreateAppController.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -71,8 +72,7 @@ public class CreateAppController implements Initializable {
     private ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
     private Appointment app;
     
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb)  {
         initTimes();
@@ -158,6 +158,16 @@ public class CreateAppController implements Initializable {
        
        System.out.println(addAppString);
                
+    }
+    
+    @FXML
+    private void cancel(){
+        try{
+             AnchorPane pane = (AnchorPane)FXMLLoader.load(getClass().getResource("/views/SelectToMakeApp.fxml"));
+            root.getChildren().setAll(pane);
+        }catch(Exception ex){
+        System.out.print(ex);
+        }
     }
     
     @FXML
